8e7e7b515bc792b94e66d19bf7ffe7fdf9f84433
Nurdin Osmonov <oooooxg@gmail.com>
2022-11-11 13:52:33 +0600

    Task completed
    UserItem:
    Added default user image to user info block.

--- src/components/Users/UserItem.tsx
+++ src/components/Users/UserItem.tsx
@@ -6,11 +6,17 @@ interface Props {
 }
 
 const UserItem: React.FC<Props> = ({user}) => {
+  const imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6xSz0eMW7GmpKukczOHvPWWGDqaBCqWA-Mw&usqp=CAU';
+  const image = imageUrl;
+  const imageStyle = {
+    background: `url(${image}) no-repeat center center / 50%`
+  }
+
   return (
     <div>
       <div className="card mb-2">
         <div className="row no-gutters">
-          <div className="col-sm-4 rounded-start"/>
+          <div className="col-sm-4 rounded-start" style={imageStyle}/>
           <div className="col-sm-8">
             <div className="card-body">
               <h5 className="card-title">{user.name}</h5>
