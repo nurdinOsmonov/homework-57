3519fc262e8a68c80394c6e034a0c33cc2b3e0af
Nurdin Osmonov <oooooxg@gmail.com>
2022-11-11 12:02:37 +0600

    Created draft layout
    removed unnecessary files.
    created components: UserForm, Users, UserItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserForm from "./components/UserForm/UserForm";
+import Users from "./components/Users/Users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="container-fluid">
+        <div className="row mt-2">
+
+          <div className="col">
+            <UserForm/>
+          </div>
+
+          <div className="col">
+            <Users/>
+          </div>
+
+        </div>
+      </main>
+    </>
   );
 }
 
